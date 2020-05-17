1bf0375994adc19f84e2f5a24d64bf3144a66ee5
Victor <nguyentanthang9a4@gmail.com>
2020-05-17 09:05:14 +0700

    Version 1.1.0 - Disable the redux devtool

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
 
+/*
 const store = createStore(rootReducer, {}, compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )); 
+*/
+const store = createStore(rootReducer, {}, applyMiddleware(thunk)); 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
